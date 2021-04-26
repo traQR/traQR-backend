8bfd6f83e6bada33dcae5fe30de2fb7ef805a3a1
Abhilash-Yannam <73598002+Abhilash-Yannam@users.noreply.github.com>
2021-04-26 23:23:17 +0530

    Authentication init

---
+++ app.js
@@ -0,0 +1,27 @@
+//jshint esversion:6
+require('dotenv').config()
+
+const express = require("express");
+
+const admin = require("firebase-admin");
+
+
+const app = express();
+
+app.use(express.urlencoded());
+
+var serviceAccount = require("C:\Users\abhil\Documents\firebase_auth\traqr-4b1c8-firebase-adminsdk-h045g-e37a5ebc62.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});;
+
+app.post("/oauth", function(req, res){
+  const idToken = req.body.id;
+  const decryptedToken = admin.verifyIdToken(idToken);
+  res.send(decryptedToken);
+})
+
+app.listen(3000, function(req, res){
+
+});
