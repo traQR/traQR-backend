f2a50aefe3a6ae6c0140c142d0700b0e6f96d691
V41SH <vaishnavsrinidhi@gmail.com>
2021-05-06 14:11:18 +0530

    added cors

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ require("dotenv").config();
 
 const express = require("express");
 
+var cors = require("cors");
+
 const admin = require("firebase-admin");
 
 const app = express();
 
 app.use(express.json());
+
+app.use(cors());
 
 app.use(
   express.urlencoded({
