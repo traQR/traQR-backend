b53d5da9e49c6f61f974914c230a2d7903cf2de9
Abhilash-Yannam <73598002+Abhilash-Yannam@users.noreply.github.com>
2021-05-15 10:16:15 +0530

    cors fix

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const express = require("express");
 
-var cors = require("cors");
+// var cors = require("cors");
 
 const admin = require("firebase-admin");
 
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 
-app.use(cors());
+// app.use(cors());
 
 app.use(
   express.urlencoded({
